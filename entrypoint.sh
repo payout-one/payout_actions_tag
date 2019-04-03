07d3770fb315091c14f2e422e4d8da8a807bff60
Oliver Kriska <oliver.kriska@payout.one>
2019-04-03 11:52:55 +0200

    Init commit with base setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-#GITHUB_EVENT_PATH="./event.json"
 
+set -e
+
+#GITHUB_EVENT_PATH="./event.json"
+#GITHUB_SHA=ef8a9445243bb0113e5dade4e97b54aef1bb7aad
+
+# Get build number from CircleCI url
 BUILD_NUMBER=$(jq '.target_url' < "$GITHUB_EVENT_PATH"  | sed 's/[^0-9]*//g')
-echo $BUILD_NUMBER
-git tag -a "1.1.${BUILD_NUMBER}"
+
+# Create with arg and build number
+TAG="${1}${BUILD_NUMBER}"
+
+# Tag commit
+git tag -a $TAG $GITHUB_SHA -m "GitHub Actions automated tag ${BUILD_NUMBER}"
+# Push commit
+git push origin $TAG
