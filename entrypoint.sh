b2bf1f9c656c8b0121d46546da8da068cbc49fa2
Oliver Kriska <oliver.kriska@payout.one>
2019-04-03 13:20:48 +0200

    Entrypoint  - fixed getting state with multiple "

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 
 # Get build number from CircleCI url
 BUILD_NUMBER=$(jq '.target_url' < "$GITHUB_EVENT_PATH"  | sed 's/[^0-9]*//g')
-STATE=$(jq '.state' < "$GITHUB_EVENT_PATH") | tr -d \"
+STATE=$(jq '.state' < "$GITHUB_EVENT_PATH" | tr -d \" )
 
 # Check if CI/CD finished
 if [ "${STATE}" == "success" ]
