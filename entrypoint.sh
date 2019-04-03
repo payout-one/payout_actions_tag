dc5110c90f58f1c349feac0695dcac95a443b67f
Oliver Kriska <oliver.kriska@payout.one>
2019-04-03 12:21:06 +0200

    Entrypoint  - added identity settings for git

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,20 @@ set -e
 # Get build number from CircleCI url
 BUILD_NUMBER=$(jq '.target_url' < "$GITHUB_EVENT_PATH"  | sed 's/[^0-9]*//g')
 
-# Create with arg and build number
-TAG="${1}${BUILD_NUMBER}"
+# Check if we get build number
+if [ -z "BUILD_NUMBER" ]
+then
+      echo "No build number, no tag"
+else
+    # Create with arg and build number
+    TAG="${1}${BUILD_NUMBER}"
 
-# Tag commit
-git tag -a $TAG $GITHUB_SHA -m "GitHub Actions automated tag ${BUILD_NUMBER}"
-# Push commit
-git push origin $TAG
+    # Set git config
+    git config --global user.email "tech@payout.one"
+    git config --global user.name "Payout Github Actions"
+
+    # Tag commit
+    git tag -a $TAG $GITHUB_SHA -m "GitHub Actions automated tag ${BUILD_NUMBER}"
+    # Push commit
+    git push origin $TAG
+fi
